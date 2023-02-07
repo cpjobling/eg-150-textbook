970c18a98445d988190c16a33ce39d9423781ec3
cpjobling <cpjobling@gpjobling.net>
2023-02-07 08:43:20 +0000

    Update documentation

--- signals_and_systems/elementary_signals/plot_heaviside.m
+++ signals_and_systems/elementary_signals/plot_heaviside.m
@@ -1,3 +1,4 @@
 syms t
-fplot(heaviside(t),[-1,1],'LineWidth',2),grid,ylim([0 1.25])
+fplot(heaviside(t),[-1,1],'LineWidth',2),grid,ylim([0 1.25]),...
+title('The Heaviside function $$u_0(t)$$','interpreter','latex'),xlabel('t')
 heaviside(0)
